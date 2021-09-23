8ddd38530d61ec3dd95cc32adc506ad5dc386579
rajeevteejwal <rajeevteejwal@users.noreply.github.com>
2021-09-23 14:24:22 +0530

    model save and load code done

---
+++ and.py
@@ -0,0 +1,34 @@
+from utils.model import perceptron
+from utils.all_utils import prepare_data, save_model, get_model
+import pandas as pd
+import os
+import logging
+import numpy as np
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+
+
+AND = {
+    "x1": [0,0,1,1],
+    "x2": [0,1,0,1],
+    "y": [0,0,0,1]
+}
+
+df = pd.DataFrame(AND)
+print(df)
+
+x,y = prepare_data(df)
+ETA = 0.3
+EPOCHS = 100
+model = perceptron(eta= ETA,epochs= EPOCHS)
+
+model.fit(x,y)
+save_model(model,"and_model")
+
+#fetch model then predict
+saved_model = get_model("and_model")
+input = np.array([[0,0]])
+print(saved_model.predict(x=input))
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -3,8 +3,9 @@ import joblib
 import numpy as np
 import pandas as pd
 import logging
+from utils.model import perceptron
 
-def prepareData(df):
+def prepare_data(df):
     """[summary]
 
     Args:
@@ -14,3 +15,15 @@ def prepareData(df):
     x = df.drop("y", axis=1)
     y = df["y"]
     return x, y
+
+def save_model(model,file_name):
+    model_dir = "models"
+    os.makedirs(model_dir,exist_ok=True)
+    file_path = os.path.join(model_dir,file_name)
+    joblib.dump(model, file_path)
+
+def get_model(file_name):
+    model_dir = "models"
+    model_path = os.path.join(model_dir,file_name)
+    model = joblib.load(model_path)
+    return model
