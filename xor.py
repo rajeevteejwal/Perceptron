28e661181525f167366dc6da6392a50ce9638a38
rajeevteejwal <rajeevteejwal@users.noreply.github.com>
2021-09-24 10:43:35 +0530

    add OR and XOR model

---
+++ xor.py
@@ -0,0 +1,40 @@
+from utils.model import perceptron
+from utils.all_utils import prepare_data, save_model, load_model
+import pandas as pd
+import os
+import logging
+import numpy as np
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir, "running_logs_xor.log"), level=logging.INFO, format=logging_str, filemode="a")
+
+def main(df, eta, epochs):
+    ETA = eta
+    EPOCHS = epochs
+    x, y = prepare_data(df)
+    model = perceptron(eta = ETA, epochs = EPOCHS)
+    model.fit(x, y)
+    save_model(model,"xor_model")
+    #load model then predict
+    saved_model = load_model("xor_model")
+    input = np.array([[0, 0],[0, 1],[1, 0],[1, 1]])
+    print(saved_model.predict(x=input))
+
+
+if __name__ == '__main__':
+    XOR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,0]
+    }
+    ETA = 0.3
+    EPOCHS = 10
+    df = pd.DataFrame(XOR)
+    try:
+        main(df, eta=ETA, epochs=EPOCHS)
+    except Exception as e:
+        logging.exception(f"Exception occured : {e}")
+        raise e
+    
